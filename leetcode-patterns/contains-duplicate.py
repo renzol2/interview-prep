00bf9a4265ba31e4b9b20c85cf2243ce8a1f126e
Renzo <renzol2@illinois.edu>
2021-06-24 08:17:08 -0500

    Add second solution

--- leetcode-patterns/contains-duplicate.py
+++ leetcode-patterns/contains-duplicate.py
@@ -5,5 +5,20 @@ class Solution(object):
     :rtype: bool
     """
     return len(set(nums)) < len(nums)
+
+  def containsDuplicate2(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: bool
+    """
+    s = set()
+    for n in nums:
+      if n not in s:
+        s.add(n)
+      else:
+        return True
+    return False
+        
+        
         
         
