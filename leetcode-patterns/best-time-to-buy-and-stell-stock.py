507b783de1f881bf28f64541f1351f54749a9bb3
Renzo <renzol2@illinois.edu>
2021-06-28 08:42:29 -0500

    Add solution

--- leetcode-patterns/best-time-to-buy-and-stell-stock.py
+++ leetcode-patterns/best-time-to-buy-and-stell-stock.py
@@ -1,3 +1,4 @@
+import sys
 class Solution:
   def maxProfit(self, prices: List[int]) -> int:
     max_profit = 0
@@ -7,5 +8,18 @@ class Solution:
         profit = prices[j] - prices[i]
         if profit > max_profit:
           max_profit = profit
+    
+    return max_profit
+
+class Solution2:
+  def maxProfit(self, prices: List[int]) -> int:
+    min_price = sys.maxsize
+    max_profit = 0
+    
+    for price in prices:
+      if price < min_price:
+        min_price = price
+      elif price - min_price > max_profit:
+        max_profit = price - min_price
     
     return max_profit
