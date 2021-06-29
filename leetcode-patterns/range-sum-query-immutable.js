e573ee5d16c2573b351702616d0e31cfc7b0c04c
Renzo <renzol2@illinois.edu>
2021-06-29 08:45:27 -0500

    Add solution

--- leetcode-patterns/range-sum-query-immutable.js
+++ leetcode-patterns/range-sum-query-immutable.js
@@ -1,22 +1,21 @@
 /**
  * @param {number[]} nums
  */
- var NumArray = function(nums) {
-  this.nums = []
+var PrefixSumNumArray = function (nums) {
+  this.nums = [];
   let prefixSum = 0;
   for (let num of nums) {
     prefixSum += num;
     this.nums.push(prefixSum);
   }
-  console.log(this.nums);
 };
 
-/** 
- * @param {number} left 
+/**
+ * @param {number} left
  * @param {number} right
  * @return {number}
  */
-NumArray.prototype.sumRange = function(left, right) {
+PrefixSumNumArray.prototype.sumRange = function (left, right) {
   if (left > 0 && right > 0) {
     return this.nums[right] - this.nums[left - 1];
   } else {
@@ -24,8 +23,25 @@ NumArray.prototype.sumRange = function(left, right) {
   }
 };
 
-/** 
- * Your NumArray object will be instantiated and called as such:
- * var obj = new NumArray(nums)
- * var param_1 = obj.sumRange(left,right)
+/**
+ * @param {number[]} nums
  */
+var NumArray = function (nums) {
+  this.storage = {};
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      this.storage[[i, j]] = sum;
+    }
+  }
+};
+
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  return this.storage[[left, right]];
+};
