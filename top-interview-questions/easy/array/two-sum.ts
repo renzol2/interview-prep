d5ab38b6187ed3752d70872b16efad08f739e31e
Renzo <renzol2@illinois.edu>
2021-07-29 18:51:19 -0500

    Move and add solution

---
+++ top-interview-questions/easy/array/two-sum.ts
@@ -0,0 +1,27 @@
+function twoSum(nums: number[], target: number): number[] {
+  const diffs = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    const diff = target - num;
+    if (diff === num) {
+      const duplicateIndex = nums.indexOf(num, i+1);
+      if (duplicateIndex === -1) {
+        continue;
+      } else {
+        return [i, duplicateIndex];
+      }
+    }
+    diffs[num] = diff;
+  }
+    
+  for (let i = 0; i < nums.length; i++) {
+    const value = diffs[nums[i]];
+    if (value !== undefined) {
+      if (diffs[value] === nums[i]) {
+        return [i, nums.indexOf(diffs[nums[i]])];      
+      }
+    }
+  }
+  
+  return [-1, -1];
+};
