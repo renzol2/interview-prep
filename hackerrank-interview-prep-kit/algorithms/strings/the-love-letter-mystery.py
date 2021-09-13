34e3a6a9849a1858d7ed3483f437597ab55ed6d7
Renzo Ledesma <renzol2@illinois.edu>
2021-09-13 08:27:08 -0500

    Add solution

---
+++ hackerrank-interview-prep-kit/algorithms/strings/the-love-letter-mystery.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+
+def theLoveLetterMystery(s):
+    ops = 0
+    for i in range(math.ceil(len(s) / 2)):
+        ops += abs(ord(s[i]) - ord(s[-1-i]))
+    return ops
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
